de4618cb1bc57a0a7889b952235c8a9e8fac8934
DornazNamazi <dornaz82n@gmail.com>
2024-11-11 17:23:06 -0500

    initisl commit

---
+++ EcommerceAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EcommerceAPI
+//
+//  Created by user268114 on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EcommerceAPI/EcommerceAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAPIApp.swift
+//  EcommerceAPI
+//
+//  Created by user268114 on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
